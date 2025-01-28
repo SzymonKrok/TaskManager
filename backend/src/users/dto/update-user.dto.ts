844407ce0aaa8890cc7cd92808d353b3c95808f3
Szymon Krok <szymonkrok49@gmail.com>
2025-01-28 22:13:48 +0100

    Added CRUD and prepared a service for the user

--- backend/src/users/dto/update-user.dto.ts
+++ backend/src/users/dto/update-user.dto.ts
@@ -1,4 +1,32 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString, IsNotEmpty, IsEmail, IsEnum, IsOptional } from 'class-validator';
+import { Role } from '../entities/user.entity';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto{
+    @ApiProperty({ description: 'Imię użytkownika', example: 'John' })
+      @IsString()
+      @IsNotEmpty()
+      firstName: string;
+
+      @ApiProperty({ description: 'Nazwisko użytkownika', example: 'Doe' })
+      @IsString()
+      @IsNotEmpty()
+      lastName: string;
+
+      @ApiProperty({ description: 'Email użytkownika', example: 'john@doe.com' })
+      @IsEmail()
+      @IsNotEmpty()
+      email: string;
+
+      @ApiProperty({ description: 'Hasło użytkownika', example: 'password' })
+      @IsString()
+      @IsNotEmpty()
+      password: string;
+
+      @ApiProperty({ example: Role.ADMIN, enum: Role, description: 'Rola uzytkownika' })
+      @IsEnum(Role)
+      @IsOptional()
+      role?: Role;
+}
