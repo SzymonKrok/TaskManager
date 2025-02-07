bd700c671ee8f4cdade8da2344fe4cdb779a2830
Szymon Krok <szymonkrok49@gmail.com>
2025-02-07 15:17:31 +0100

    Created basic CRUD

--- backend/src/tasks/tasks.module.ts
+++ backend/src/tasks/tasks.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { TasksController } from './tasks.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Task } from './entities/task.entity';
+import { User } from 'src/users/entities/user.entity';
 
 @Module({
   controllers: [TasksController],
   providers: [TasksService],
+  imports: [TypeOrmModule.forFeature([Task, User])],
 })
 export class TasksModule {}
--- backend/src/tasks/tasks.service.ts
+++ backend/src/tasks/tasks.service.ts
@@ -1,26 +1,84 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
+import { Repository } from 'typeorm';
+import { Task } from './entities/task.entity';
+import { NotFoundError } from 'rxjs';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/users/entities/user.entity';
 
 @Injectable()
 export class TasksService {
-  create(createTaskDto: CreateTaskDto) {
-    return 'This action adds a new task';
+  constructor(
+    @InjectRepository(Task)
+    private readonly tasksRepository: Repository<Task>,
+
+    @InjectRepository(User)
+    private readonly usersRepository: Repository<User>,
+  ) {}
+  async create(createTaskDto: CreateTaskDto): Promise<Task> {
+    const { user_id, ...rest } = createTaskDto;
+
+
+
+    // tutaj normalnie pobierasz usera
+    let user = await this.usersRepository.findOne({
+      where: { id: user_id },
+    });
+
+    if (!user_id) {
+      user = null
+    }
+
+    // if (!user) {
+    //   throw new NotFoundException(`User with id ${user_id} not found`);
+    // }
+
+    // tworzysz Taska
+    const task = this.tasksRepository.create({
+      ...rest,
+      user,
+    });
+
+    return this.tasksRepository.save(task);
   }
 
-  findAll() {
-    return `This action returns all tasks`;
+  async findAll(): Promise<Task[]> {
+    const tasks = await this.tasksRepository.find({
+      relations: ['user'],
+    });
+
+    // tasks.map()
+
+    return tasks;
+
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} task`;
+  async findOne(id: number): Promise<Task | null> {
+    const task = await this.tasksRepository.findOne({ where: { id } });
+
+    return task;
   }
 
-  update(id: number, updateTaskDto: UpdateTaskDto) {
-    return `This action updates a #${id} task`;
+  async update(id: number, request: UpdateTaskDto): Promise<Task> {
+    const task = await this.tasksRepository.findOne({ where: { id }});
+
+    if (!task) {
+      throw new NotFoundException(`Task with id ${id} not found`);
+    }
+
+    await this.tasksRepository.update(id, request);
+    return task;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} task`;
+  async remove(id: number): Promise<Task> {
+    const task = await this.findOne(id);
+
+    if (!task) {
+      throw new NotFoundException(`Task with id ${id} not found`);
+    }
+
+    await this.tasksRepository.remove(task);
+    return task;
   }
 }
